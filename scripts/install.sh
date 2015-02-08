88b6e558fb44c98733b2e9adb32da7d0434af050
David Bräutigam <david.braeutigam@gmail.com>
2015-02-08 15:14:47 +0000

    Configs behalten

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,16 +2,17 @@
 
 # copy the files
 cp ../src/vpn_switcher.py /usr/bin/vpn_switcher 
-mkdir /etc/vpn_switcher
-cp ../config/vpn_switcher.config /etc/vpn_switcher/vpn_switcher.config
-cp vpn_switcher.starter /etc/init.d/vpn_switcher
-
-
-# set the owner and the restrictions
 chown root:root /usr/bin/vpn_switcher
-chown root:root	/etc/vpn_switcher/vpn_switcher.config
-chown root:root /etc/init.d/vpn_switcher
-
 chmod 777 /usr/bin/vpn_switcher
-chmod 644 /etc/vpn_switcher/vpn_switcher.config
+
+if [ ! -f /etc/vpn_switcher/vpn_switcher.config ]
+then
+	mkdir /etc/vpn_switcher
+	cp ../config/vpn_switcher.config /etc/vpn_switcher/vpn_switcher.config
+	chown root:root	/etc/vpn_switcher/vpn_switcher.config
+	chmod 644 /etc/vpn_switcher/vpn_switcher.config
+fi
+
+cp vpn_switcher.starter /etc/init.d/vpn_switcher
+chown root:root /etc/init.d/vpn_switcher
 chmod 755 /etc/init.d/vpn_switcher
--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -5,5 +5,4 @@ killall vpn_switcher
 
 # remove the file and folders
 rm -f /usr/bin/vpn_switcher
-rm -rf /etc/vpn_switcher
 rm -f /etc/init.d/vpn_switcher
